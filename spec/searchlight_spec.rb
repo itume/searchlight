ebda51bb7966336e709c7f18d22ddf8ea93fe195
itume <jem90ham@gmail.com>
2019-07-07 10:46:11 +0900

    initial commit

---
+++ spec/searchlight_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Searchlight do
+  it "has a version number" do
+    expect(Searchlight::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
